eb5c46437d455e09ea57569e3fc51384c3571f8a
Sachin K Subramanya <sachin.subramanya@gmail.com>
2014-08-20 22:55:25 +0530

    version 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+#makeCacheMatrix -> stores the chache of Matrix and Iverse
+#cacheSolve -> compute Inverse matrix if cached value is NULL,else compute the Inverse
 
-## Write a short comment describing this function
 
+## cache the matrix and return a list 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  mcache <<- x;
+  mInv<<-NULL;
+  
+  get <- function() {mcache}
+  getInv<-function(){mInv}
+  solveInv<-function(y) {
+    mInv<<-solve(y);
+    mInv
+  }
+  list( get = get,getInv= getInv, solve =solveInv);
+  
+} 
 
-}
 
-
-## Write a short comment describing this function
+## retrun the inverse matrix from cache ,else compute 
+##  and store it in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  x1<-x$getInv();
+  if(!is.null(x1)){
+    print("returning cache..");
+    return (x1);
+  }else{
+    
+    data<-x$get();
+    print("solving inv..")
+    
+    x2<-x$solve(data) 
+    return (x2)
+    
+  }
+  
 }
